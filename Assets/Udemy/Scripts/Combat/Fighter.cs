172561bbdb82211959534c3a8b907a3837482cfe
senseImamuraY <iy5035704@gmail.com>
2022-10-12 21:34:35 +0900

    fin 36

--- Assets/Udemy/Scripts/Combat/Fighter.cs
+++ Assets/Udemy/Scripts/Combat/Fighter.cs
@@ -9,10 +9,16 @@ namespace RPG.Combat
   public class Fighter : MonoBehaviour, IAction
   {
     [SerializeField] float weaponRange = 2f;
+    [SerializeField] float timeBetweenAttacks = 1f;
+    [SerializeField] float weaponDamage = 5f;
+
     Transform target;
+    float timeSinceLastAttack = 0;
     
     private void Update()
     {
+      timeSinceLastAttack += Time.deltaTime;
+
       if (target == null) return;
       //bool isInRange = GetIsInRange();
       // null‚Ìê‡‚ÍŠÖ”‚ğŒÄ‚Î‚È‚¢‚Ì‚ÅƒGƒ‰[‚ğ‰ñ”ğ‚Å‚«‚éB
@@ -23,15 +29,36 @@ namespace RPG.Combat
       else
       {
         GetComponent<Mover>().Cancel();
-        //Cancel();
-      }
+        AttackBehaviour();
+        
 
-      bool GetIsInRange()
-      {
-        //Debug.Log(target);
-        return Vector3.Distance(transform.position, target.position) < weaponRange;
       }
     }
+
+    private void AttackBehaviour()
+    {
+      if(timeSinceLastAttack > timeBetweenAttacks)
+      {
+        // This will trigger the Hit() event.
+        GetComponent<Animator>().SetTrigger("attack");
+        timeSinceLastAttack = 0;
+
+      }
+    }
+
+    // Animation Event
+    void Hit()
+    {
+      Health healthComponent = target.GetComponent<Health>();
+      healthComponent.TakeDamage(weaponDamage);
+    }
+
+    bool GetIsInRange()
+    {
+      //Debug.Log(target);
+      return Vector3.Distance(transform.position, target.position) < weaponRange;
+    }
+
     public void Attack(CombatTarget combatTarget)
     {
       GetComponent<ActionScheduler>().StartAction(this);
@@ -42,5 +69,7 @@ namespace RPG.Combat
     {
       target = null;
     }
+
+
   }
 }
